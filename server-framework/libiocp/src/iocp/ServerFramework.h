c6c64c394f7430bb5e784c2cec38f93e05349733
Jeff Wang <wohaaitinciu@163.com>
2015-03-12 15:09:12 +0800

    rename file

--- server-framework/libiocp/src/iocp/ServerFramework.h
+++ server-framework/libiocp/src/iocp/ServerFramework.h
@@ -1,10 +1,10 @@
 #ifndef _SERVER_FRAMEWORK_H_
 #define _SERVER_FRAMEWORK_H_
 
-#include "IOCPServerFramework.h"
+#include "ServerFrameworkImpl.h"
 
 namespace iocp {
-    template <class _T = void> class ClientContext final : public _ClientContext
+    template <class _T = void> class ClientContext final : public _impl::_ClientContext
     {
     public:
         ClientContext() : _val(_T()) { }
@@ -16,18 +16,21 @@ namespace iocp {
         uint16_t getPort() const { return _port; }
     };
 
-    template <> class ClientContext<void> : public _ClientContext
+    template <> class ClientContext<void> : public _impl::_ClientContext
     {
     public:
         const char *getIp() const { return _ip; }
         uint16_t getPort() const { return _port; }
     };
 
-    template <class _T = void> class ServerFramework final : public _ServerFramework
+    template <class _T = void> class ServerFramework final : public _impl::_ServerFramework
     {
     public:
         ServerFramework() { }
         ~ServerFramework() { }
+
+        using _impl::_ServerFramework::initialize;
+        using _impl::_ServerFramework::uninitialize;
 
         typedef std::function<size_t (ClientContext<_T> *ctx, const char *buf, size_t len)> ClientRecvCallback;
         typedef std::function<void (ClientContext<_T> *ctx)> ClientDisconnectCallback;
@@ -36,14 +39,14 @@ namespace iocp {
             const ClientRecvCallback &clientRecvCallback,
             const ClientDisconnectCallback &clientDisconnectCallback)
         {
-            return _ServerFramework::startup(ip, port, [clientRecvCallback](_ClientContext *ctx, const char *buf, size_t len)->size_t {
+            return _impl::_ServerFramework::startup(ip, port, [clientRecvCallback](_impl::_ClientContext *ctx, const char *buf, size_t len)->size_t {
                 return clientRecvCallback((ClientContext<_T> *)ctx, buf, len);
-            }, [clientDisconnectCallback](_ClientContext *ctx) {
+            }, [clientDisconnectCallback](_impl::_ClientContext *ctx) {
                 return clientDisconnectCallback((ClientContext<_T> *)ctx);
             });
         }
 
-        using _ServerFramework::shutdown;
+        using _impl::_ServerFramework::shutdown;
 
     private:
         ServerFramework(const ServerFramework &) = delete;
